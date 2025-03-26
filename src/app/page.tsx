aa9e02b119fec8b78567c02c670d8d5a4330d4c4
charan chunchu <ch.charan98@gmail.com>
2025-03-27 02:55:08 +0530

    change erroes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { useState, useEffect, useCallback } from "react"
-import { Button } from "@/components/ui/button"
 import Image from "next/image"
 import Link from "next/link"
 import { motion } from "framer-motion"
